47695d883aa9588ee5eab61cb3a8bc6e7fd88559
Carlos Gamino <carlos.alfredo55@gmail.com>
2022-12-12 19:15:29 -0600

    Added eslint hooks react

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,24 +1,22 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:@tanstack/eslint-plugin-query/recommended'
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
-  'overrides': [],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint'
-  ],
-  'rules': {}
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
 };
