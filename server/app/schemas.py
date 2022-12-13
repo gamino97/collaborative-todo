aac526de4f4358d5332945b4b28c2b633532ac04
Carlos Gamino <carlos.alfredo55@gmail.com>
2022-12-12 19:16:48 -0600

    Added Tasks Schemas

--- server/app/schemas.py
+++ server/app/schemas.py
@@ -1,6 +1,25 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import EXCLUDE, fields, validate
+
+from .ma import ma
+from .models import Task
 
 
-class CreateTaskSchema(Schema):
+class CreateTaskSchema(ma.Schema):
     title = fields.Str(required=True, validate=[validate.Length(max=255)])
     description = fields.Str(required=False, validate=[validate.Length(max=1024)])
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+class TaskSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Task
+        unknown = EXCLUDE
+
+    id = ma.auto_field(dump_only=True)
+    title = ma.auto_field()
+    description = ma.auto_field()
+    done = ma.auto_field()
+    created_at = ma.auto_field(dump_only=True)
+    team_id = ma.auto_field()
