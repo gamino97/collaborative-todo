f3eaf9156e8c67ed131ca2011396f9e8984605a0
Carlos Gamino <carlos.alfredo55@gmail.com>
2023-01-20 18:45:17 -0600

    Add lazy to BaseTabsLayout

--- client/src/components/BaseTabsLayout.tsx
+++ client/src/components/BaseTabsLayout.tsx
@@ -16,7 +16,7 @@ export default function BaseTabsLayout({ children }: Props) {
   return (
     <Box>
       <Container maxW="container.lg" marginTop="20px">
-        <Tabs isFitted variant="soft-rounded" colorScheme="blue">
+        <Tabs isFitted variant="soft-rounded" colorScheme="blue" isLazy>
           <TabList mb="1em">
             <Tab>My Tasks</Tab>
             <Tab>My Team</Tab>
