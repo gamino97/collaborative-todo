968629668f055e8e1fbefb1f6582fc1a131d86ac
Carlos Gamino <carlos.alfredo55@gmail.com>
2023-01-20 18:52:27 -0600

    Add emacs files

---
+++ client/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((tsx-mode . ((format-all-formatters . (("TSX" . (prettier))))
+              (eval . (format-all-mode))))
+ (typescript-mode . ((format-all-formaters . (("Typescript" . (prettier))))
+                     (eval . (format-all-mode)))))
---
+++ server/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (pyvenv-workon "collaborative-todo"))
+         (gm-flask-commands-app-name . "app.main"))))
