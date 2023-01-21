0a496e118ab8d4a55ae1f1b17407c8ac9511915b
Carlos Gamino <carlos.alfredo55@gmail.com>
2023-01-20 18:54:19 -0600

    Add myteam endpoint and add logic to create_team

--- server/app/teams.py
+++ server/app/teams.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_required
 from marshmallow import ValidationError
 
 from .database import db
-from .models import Team
+from .models import Team, User, TeamModel
 from .schemas import TeamSchema
 
 bp = Blueprint("teams", __name__, url_prefix="/api/teams")
@@ -12,6 +12,9 @@ bp = Blueprint("teams", __name__, url_prefix="/api/teams")
 @bp.post("/create")
 @login_required
 def create_team():
+    user: User = current_user
+    if user.team_id:
+        return {"message": "Invalid user"}, 400
     request_data = request.get_json()
     team_schema = TeamSchema()
     try:
@@ -20,8 +23,18 @@ def create_team():
         return err.messages.copy(), 400
     else:
         name: str = result.get("name")
-        task = Team(name=name)
-        db.session.add(task)
-        task.users.append(current_user)
+        team = Team(name=name)
+        db.session.add(team)
+        team.users.append(current_user)
         db.session.commit()
-        return team_schema.dump(task), 201
+        return team_schema.dump(team), 201
+
+
+@bp.get("/myteam")
+@login_required
+def my_team():
+    user = current_user
+    if not user.team_id:
+        return {"message": "No team is associated with this user"}
+    team_schema = TeamSchema()
+    return team_schema.dump(current_user.team)
