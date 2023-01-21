78e9c7ed6d505f1662e3d145990c12662c344fea
Carlos Gamino <carlos.alfredo55@gmail.com>
2023-01-20 18:51:34 -0600

    First commit of teams component

--- client/src/components/Teams.tsx
+++ client/src/components/Teams.tsx
@@ -1,11 +1,12 @@
-import {
-  Heading,
-  Center,
-} from "@chakra-ui/react";
+import { Heading, Center } from "@chakra-ui/react";
 import { CreateTeam } from "components/CreateTeam";
 import { JoinTeam } from "components/JoinTeam";
+import { useTeam } from "services/team";
+import Fallback from "components/Fallback";
+import QueryError from "components/QueryError";
+import NoTask from "components/NoTask";
 
-export default function Teams() {
+export function NoTeam() {
   return (
     <>
       <Center>
@@ -13,13 +14,32 @@ export default function Teams() {
       </Center>
       <Center>
         <Heading as="h2" size="xl">
-          You are not part of a team.
+          You are not associated with any team.
         </Heading>
       </Center>
       <Center gap={4} mt={4}>
         <CreateTeam />
         <JoinTeam />
       </Center>
+    </>
+  );
+}
+
+export default function Teams() {
+  const { isLoading, error, data: team } = useTeam();
+  if (isLoading || !team) return <Fallback />;
+  if (error instanceof Error) return <QueryError error={error} />;
+  if (team.message) return NoTeam;
+  console.log({ tasks: team.tasks });
+  const teamTasks = team.tasks;
+  return (
+    <>
+      <Center>
+        <Heading as="h2" size="2xl">
+          {team.name}
+        </Heading>
+      </Center>
+      {teamTasks.length === 0 ? <NoTask team /> : "Hola xd"}
     </>
   );
 }
